fe3ffd984dfc2a2cc32b5ba61bf51641ccd6e271
Rashid <rashidstanikzai.net@gmail.com>
2022-09-28 14:27:26 +0500

    recipe list added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe',
+      'It is a test recipe bro',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS85AyYyNtyzG2-FvmzYNBLh8jnNcC_cYAFNA&usqp=CAU'
+    ),
+    new Recipe(
+      'Test recipe',
+      'It is a test recipe bro',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS85AyYyNtyzG2-FvmzYNBLh8jnNcC_cYAFNA&usqp=CAU'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
