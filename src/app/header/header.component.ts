a7ccb33d316842f1b9509c26e6bb0edd1d640502
Rashid <rashidstanikzai.net@gmail.com>
2022-10-03 19:48:25 +0500

    navigation implemented
    Though there are some fixes to styling too

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() viewChanged = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onViewChanged(currentView: string) {
+    this.viewChanged.emit(currentView);
   }
-
 }
